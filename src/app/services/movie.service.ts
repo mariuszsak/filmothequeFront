35e9f0853cd6a3617f6d8ebe2b57eed9c8a89a70
thornhell <mariuszsak@gmail.com>
2019-07-04 20:39:00 +0200

    Minor code fixing

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,19 +1,19 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {Movie} from '../model/movie.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MovieService {
 
-  private serviceUrl = 'http://localhost:8080/all';
+    private serviceUrl = 'http://localhost:8080/all';
 
-  constructor(private http: HttpClient) {
-  }
+    constructor(private http: HttpClient) {
+    }
 
-  getMovie(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(this.serviceUrl);
-  }
+    getMovie(): Observable<Movie[]> {
+        return this.http.get<Movie[]>(this.serviceUrl);
+    }
 }
