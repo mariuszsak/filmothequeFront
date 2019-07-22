0bc333f0c46554d64e71d233b847362713cf3a74
mariuszsak <mariuszsak@gmail.com>
2019-07-22 19:09:00 +0200

    Register form & Code cleaning

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {AuthLoginInfo} from './login-info';
 import {Observable} from 'rxjs';
 import {JwtResponse} from './jwt-response';
+import {AuthRegisterInfo} from './register-info';
 
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -14,11 +15,16 @@ const httpOptions = {
 export class AuthService {
 
     private loginUrl = 'http://localhost:8080/login';
+    private signupUrl = 'http://localhost:8080/signup';
 
     constructor(private http: HttpClient) {
     }
 
     attemptAuth(credentials: AuthLoginInfo): Observable<JwtResponse> {
         return this.http.post<JwtResponse>(this.loginUrl, credentials, httpOptions);
+    }
+
+    signUp(info: AuthRegisterInfo): Observable<string> {
+        return this.http.post<string>(this.signupUrl, info, httpOptions);
     }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../authentication/auth.service';
+import {AuthRegisterInfo} from '../authentication/register-info';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+    form: any = {};
+    signupInfo: AuthRegisterInfo;
+    isSignedUp = false;
+    isSignUpFailed = false;
+    errorMessage = '';
 
-  ngOnInit() {
-  }
+    constructor(private authService: AuthService) {
+    }
+
+    ngOnInit() {
+    }
+
+    onSubmit() {
+        console.log(this.form);
+
+        this.signupInfo = new AuthRegisterInfo(
+            this.form.username,
+            this.form.password);
+
+        this.authService.signUp(this.signupInfo).subscribe(
+            data => {
+                console.log(data);
+                this.isSignedUp = true;
+                this.isSignUpFailed = false;
+            },
+            error => {
+                console.log(error);
+                this.errorMessage = error.error.message;
+                this.isSignUpFailed = true;
+            }
+        );
+    }
 
 }
