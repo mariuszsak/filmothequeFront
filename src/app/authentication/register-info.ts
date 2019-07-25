721c9d5d09c63526983e75eae3940e219b46947c
mariuszsak <mariuszsak@gmail.com>
2019-07-25 20:32:01 +0200

    userSettings added

--- src/app/authentication/register-info.ts
+++ src/app/authentication/register-info.ts
@@ -1,11 +1,17 @@
+import {Settings} from '../model/settings.model';
+
+
 export class AuthRegisterInfo {
     username: string;
     role: string[];
     password: string;
+    userSettings: Settings;
 
-    constructor(username: string, password: string) {
+
+    constructor(username: string, password: string, settings: Settings) {
         this.username = username;
         this.password = password;
         this.role = ['user'];
+        this.userSettings = settings;
     }
 }
